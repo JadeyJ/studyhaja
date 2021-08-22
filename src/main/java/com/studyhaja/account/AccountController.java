b7d25a601bb058242f99b7eb71ec5ce7066eb2b9
Geunhee Kim <geunheekiim@gmail.com>
2021-08-22 21:47:52 +0900

    회원가입 리팩토링

--- src/main/java/com/studyhaja/account/AccountController.java
+++ src/main/java/com/studyhaja/account/AccountController.java
@@ -2,8 +2,6 @@ package com.studyhaja.account;
 
 import com.studyhaja.domain.Account;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -19,8 +17,8 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
-    private final AccountRepository accountRepository;
-    private final JavaMailSender javaMailSender;
+    private final AccountService accountService;
+
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder){
@@ -36,30 +34,11 @@ public class AccountController {
     @PostMapping("/sign-up")
     public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
         if(errors.hasErrors()){
-            System.out.println("에러있음");
             return "account/sign-up";
         }
-        System.out.println("에러없음");
-        // TODO: 회원 가입 처리
-        Account account = Account.builder()
-                .email(signUpForm.getEmail())
-                .nickname(signUpForm.getNickname())
-                .password(signUpForm.getPassword())     // TODO: 비밀번호 인코딩 해야함
-                .studyCreatedByWeb(true)
-                .studyEnrollmentResultByWeb(true)
-                .studyUpdatedByWeb(true)
-                .build();
-        Account newAccount = accountRepository.save(account);
-
-        // TODO: 이메일 체크 토큰 생성
-        newAccount.generateEmailCheckToken();
-
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(newAccount.getEmail());
-        mailMessage.setSubject("스터디하자 가입 인증 메일");
-        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() + "&email=" + newAccount.getEmail());
-        javaMailSender.send(mailMessage);
-
+        accountService.processNewAccount(signUpForm);
         return "redirect:/";
     }
+
+
 }
