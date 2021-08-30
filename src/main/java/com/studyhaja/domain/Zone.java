3eeaf6e7eca2e425f92ceb0075f9fdeb8906c4f2
Geunhee Kim <geunheekiim@gmail.com>
2021-08-30 15:01:13 +0900

    지역 도메인

---
+++ src/main/java/com/studyhaja/domain/Zone.java
@@ -0,0 +1,27 @@
+package com.studyhaja.domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @NoArgsConstructor @AllArgsConstructor
+public class Zone {
+
+    @Id @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String localNameOfCity;
+
+    @Column(nullable = true)
+    private String province;
+
+}
---
+++ src/main/java/com/studyhaja/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.studyhaja.zone;
+
+import com.studyhaja.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
