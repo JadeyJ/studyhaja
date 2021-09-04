619873f1bfbea0cc4cb72fcc79d9700b3fce7d8e
Geunhee Kim <geunheekiim@gmail.com>
2021-09-04 23:34:58 +0900

    스터디 설명 수정 테스트

---
+++ src/test/java/com/studyhaja/study/StudySettingsControllerTest.java
@@ -0,0 +1,19 @@
+package com.studyhaja.study;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+class StudySettingsControllerTest {
+
+    @Autowired MockMvc mockMvc;
+
+    
+}
