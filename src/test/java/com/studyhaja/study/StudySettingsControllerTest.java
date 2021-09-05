6721ba12a2a9d5db87f251e5d3d51ce751a8861e
Geunhee Kim <geunheekiim@gmail.com>
2021-09-05 23:27:17 +0900

    스터디 설명 수정 테스트

--- src/test/java/com/studyhaja/study/StudySettingsControllerTest.java
+++ src/test/java/com/studyhaja/study/StudySettingsControllerTest.java
@@ -1,12 +1,19 @@
 package com.studyhaja.study;
 
+import com.studyhaja.WithAccount;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.studyhaja.settings.SettingsController.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 @Transactional
 @SpringBootTest
@@ -15,5 +22,10 @@ class StudySettingsControllerTest {
 
     @Autowired MockMvc mockMvc;
 
-    
+    @WithAccount("kimmy")
+    @DisplayName("스터디 설명 수정 폼")
+    @Test
+    void updateStudyInfoForm() throws Exception {
+        
+    }
 }
