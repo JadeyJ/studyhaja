2b31a7c87c6561a015b69d53733ff613dcd245cd
Geunhee Kim <geunheekiim@gmail.com>
2021-09-16 07:35:49 +0900

    알림 아이콘 수정

--- src/main/java/com/studyhaja/modules/notification/NotificationRepository.java
+++ src/main/java/com/studyhaja/modules/notification/NotificationRepository.java
@@ -1,6 +1,10 @@
 package com.studyhaja.modules.notification;
 
+import com.studyhaja.modules.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    long countByAccountAndChecked(Account account, boolean checked);
+
 }
