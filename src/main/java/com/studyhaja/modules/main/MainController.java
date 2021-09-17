011b55bf671c6c88fd1a63f4d2ff7bea46421d6d
Geunhee Kim <geunheekiim@gmail.com>
2021-09-17 16:50:52 +0900

    검색 기능

--- src/main/java/com/studyhaja/modules/main/MainController.java
+++ src/main/java/com/studyhaja/modules/main/MainController.java
@@ -2,12 +2,20 @@ package com.studyhaja.modules.main;
 
 import com.studyhaja.modules.account.CurrentUser;
 import com.studyhaja.modules.account.Account;
+import com.studyhaja.modules.study.Study;
+import com.studyhaja.modules.study.StudyRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+
+    private final StudyRepository studyRepository;
 
     @GetMapping("/")
     public String home(@CurrentUser Account account, Model model) {
@@ -20,5 +28,13 @@ public class MainController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/search/study")
+    public String searchStudy(String keyword, Model model) {
+        List<Study> studyList = studyRepository.findByKeyword(keyword);
+        model.addAttribute(studyList);
+        model.addAttribute("keyword", keyword);
+        return "search";
     }
 }
