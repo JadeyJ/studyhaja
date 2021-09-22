2b5c3f170461df9c245176b0fc566384c32b0ec7
Geunhee Kim <geunheekiim@gmail.com>
2021-09-22 21:40:27 +0900

    페이징 적용

--- src/main/java/com/studyhaja/modules/main/MainController.java
+++ src/main/java/com/studyhaja/modules/main/MainController.java
@@ -1,15 +1,17 @@
 package com.studyhaja.modules.main;
 
-import com.studyhaja.modules.account.CurrentUser;
 import com.studyhaja.modules.account.Account;
+import com.studyhaja.modules.account.CurrentUser;
 import com.studyhaja.modules.study.Study;
 import com.studyhaja.modules.study.StudyRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -31,9 +33,9 @@ public class MainController {
     }
 
     @GetMapping("/search/study")
-    public String searchStudy(String keyword, Model model) {
-        List<Study> studyList = studyRepository.findByKeyword(keyword);
-        model.addAttribute(studyList);
+    public String searchStudy(String keyword, Model model, @PageableDefault(size = 9, sort = "publishedDateTime", direction = Sort.Direction.DESC)Pageable pageable) {
+        Page<Study> studyPage = studyRepository.findByKeyword(keyword, pageable);
+        model.addAttribute("studyPage", studyPage);
         model.addAttribute("keyword", keyword);
         return "search";
     }
